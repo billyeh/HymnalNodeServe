122580d58d8077fbdee09ec9320275c102c500f0
= <billwyeh@gmail.com>
2012-12-01 12:10:42 -0800

    added content, styling to site

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,105 +1,58 @@
 var fs = require('fs');
 var querystring = require('querystring');
+var mime = require('mime');
+
+function write(error, content, response, query, contentType) {
+	if (error) {
+		response.writeHead(500);
+		response.end();
+		
+		console.log("Error reading file");
+		console.log("Query for " + query + " received");
+	}
+	else {
+		response.writeHead(200, {"Content-Type": contentType});
+		response.end(content, 'utf-8');
+		
+		console.log("Serving file");
+		console.log("Query for " + query + " received");
+	}
+}
 
 function start(response, query) {
 	fs.readFile("HymnDoc.html", function(error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-			
-			console.log("Error reading file");
-			console.log("Query for " + query + " received");
-		}
-		else {
-			response.writeHead(200);
-			response.end(content, 'utf-8');
-			
-			console.log("Serving file");
-			console.log("Query for " + query + " received");
-		}
+		write(error, content, response, query, mime.lookup("HymnDoc.html"));
 	});
-	
 }
 
 function backgroundImg(response, query) {
 	fs.readFile("nav.png", function(error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-			
-			console.log("Error reading file");
-			console.log("Query for " + query + " received");
-		}
-		else {
-			response.writeHead(200, {"Content-Type": "image/png"});
-			response.end(content, 'utf-8');
-			
-			console.log("Serving file");
-			console.log("Query for " + query + " received");
-		}
+		write(error, content, response, query, mime.lookup("nav.png"));
 	});
 }
 
-function serveFile(response, query) {
-	fs.readFile("./" + querystring.parse(query).type + "/hymn" + 
-			querystring.parse(query).hymn + ".txt", function(error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-			
-			console.log("Error reading file");
-			console.log("Query for " + query + " received");
-		}
-		else {
-			response.writeHead(200, {"Content-Type": "application/json"});
-			response.end(content, 'utf-8');
-			
-			console.log("Serving file");
-			console.log("Query for " + query + " received");
-		}
+function serveHymn(response, query) {
+	var file = "./" + querystring.parse(query).type + "/hymn" + querystring.parse(query).hymn + ".txt";
+	fs.readFile(file, function(error, content) {
+		write(error, content, respone, query, mime.lookup(file));
 	});
 }
 
 function favicon(response, query) {
 	fs.readFile("favicon.ico", function(error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-			
-			console.log("Error reading file");
-			console.log("Query for " + query + " received");
-		}
-		else {
-			response.writeHead(200, {"Content-Type": "image/x-icon"});
-			response.end(content, 'utf-8');
-			
-			console.log("Serving file");
-			console.log("Query for " + query + " received");
-		}
+		write(error, content, response, query, mime.lookup("favicon.ico"));
 	});
 }
 
 function docmainCSS(response, query) {
 	fs.readFile("docmain.css", function(error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-			
-			console.log("Error reading file");
-			console.log("Query for " + query + " received");
-		}
-		else {
-			response.writeHead(200, {"Content-Type": "text/css"});
-			response.end(content, 'utf-8');
-			
-			console.log("Serving file");
-			console.log("Query for " + query + " received");
-		}
+		write(error, content, response, query, mime.lookup("docmain.css"));
 	});
 }
 
 exports.start = start;
-exports.serveFile = serveFile;
+exports.serveHymn = serveFile;
 exports.favicon = favicon;
 exports.docmainCSS = docmainCSS;
 exports.background = backgroundImg;
+exports.write = write;
